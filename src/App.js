8e18b628e54ca2989615215d79d6fc9abc3db65a
michealccc <4558199+michealccc@users.noreply.github.com>
2020-09-30 01:20:50 +1300

    Replace placeholder logo to SEER logo in Navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import logo from './logo.svg';
 import './style.css';
-import { StarOutlined,HeartTwoTone, TrademarkCircleFilled,UserOutlined, FrownFilled} from '@ant-design/icons';
+
+import { HeartTwoTone, TrademarkCircleFilled,UserOutlined} from '@ant-design/icons';
 import { Button,Divider  } from 'antd';
 import 'antd/dist/antd.css'
 import Homepage from './Homepage.jsx';
@@ -11,14 +13,14 @@ export default class App extends React.Component{
     return(
       <div>
         <Router>
-      <Link to="/">  <div className="logo" > <StarOutlined  />SEER</div></Link>
+      <Link to="/">  <div className="logo" ><img src={logo} className="navbar-logo-brand" alt="logo"/>SEER</div></Link>
       <Link to="/save">  <div className="save"><Button icon={<HeartTwoTone />}>My saved searches</Button></div></Link>
       <Link to="/rate">  <div className="rate"><Button icon={<TrademarkCircleFilled />}>My ratings</Button></div></Link>
 
        <Link to="/login"> <div className="user"><Button icon={<UserOutlined />}>User</Button></div></Link>
         <Divider className="divider"/>
 
-        
+
         <Route path="/" exact component={Homepage}></Route>
         {/* <Route path="/login" exact component={Save}></Route>
         <Route path="/save" exact component={Rate}></Route> */}
